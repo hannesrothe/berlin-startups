c5ea5258a9724a03f207effa4cb1be884d1ce034
hannes <boundedrational@gmail.com>
2014-12-03 15:02:00 +0100

    added founder scraper

--- fetch_gruenderszene.py
+++ fetch_gruenderszene.py
@@ -1,27 +1,35 @@
 import urllib2
 import json
+import re
 from BeautifulSoup import BeautifulSoup
 from startup_class import Startup
 
+#load url, clear scraped data from <br>
 response = urllib2.urlopen('http://www.gruenderszene.de/datenbank/unternehmen/a-space-for-art')
-soup = BeautifulSoup(response)
+newResponse = re.sub('<br />', ' ', response.read())
+soup = BeautifulSoup(newResponse)
 
-#print my_table
+#fetch initial startup data
 startupName = soup.find("div", "profile-additional-information").ul.li.text
 foundedIn = soup.find("div", "profile-additional-information").ul.li.findNextSibling("li").span.text
-startupTeaser = soup.find("p", "teaser").text
 startupDescription = soup.find("p", "teaser").findNextSibling("p").text
+
+#initiate startup object
+myStartup = Startup(startupName, startupDescription, foundedIn)
+
+#additional startup information
+startupTeaser = soup.find("p", "teaser").text
+myStartup.addTeaser(startupTeaser)
+
+#add founders
 founderList = soup.findAll("div", "head-information")
 
 for founder in founderList:
-    founderName = founder.text
-    print founderName
+    founderName = founder.a.text
+    founderUrl = founder.a.get('href')
+    founderRole = founder.a.findNextSibling('p').text
+    myStartup.addFounder(founderName, founderRole, founderUrl)
 
 
-
-mystartup = Startup(startupName, startupDescription, foundedIn)
-mystartup.addTeaser(startupTeaser)
-
-
-       
-print json.dumps(mystartup.getStartupData(), sort_keys = True)
+#output data
+print json.dumps(myStartup.getStartupData(), sort_keys = True)
